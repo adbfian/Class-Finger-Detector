77ef4c511bce165cad343de662393b350bd97313
adbfian <adb.fian@gmail.com>
2016-08-15 09:29:35 +0700

    Add files via upload

---
+++ HSVDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace ClassLibrary_Detector
+{
+    public class HSVDetector:IColorAbstrak
+    {
+        //Class untuk eksekusi Hue dan Saturation pada gambar
+        public override Image<Gray, byte> DetectSkin(Image<Bgr, byte> Img, IColor min, IColor max)
+        {
+            Image<Hsv, Byte> currentHsvFrame = Img.Convert<Hsv, Byte>();
+            Image<Gray, byte> skin = new Image<Gray, byte>(Img.Width, Img.Height);
+            skin = currentHsvFrame.InRange((Hsv)min, (Hsv)max);
+            return skin;
+        }
+    }
+}
---
+++ IColorAbstrak.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace ClassLibrary_Detector
+{
+    public abstract class IColorAbstrak
+    {
+        public abstract Image<Gray, Byte> DetectSkin(Image<Bgr, Byte> Img, IColor min, IColor max);
+    }
+}
